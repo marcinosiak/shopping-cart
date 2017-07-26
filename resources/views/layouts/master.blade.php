b4b617fd2abc7799948bbf5990c7da65062f1e35
marcinosiak <om77@wp.pl>
2017-07-26 15:48:48 +0200

    Utworzenie navbara

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,10 +6,15 @@
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
     @yield('styles')
 
   </head>
   <body>
+    @include('partials.header')
+
     @yield('content')
 
     <!-- jQuery CDN – Latest Stable Versions -->
