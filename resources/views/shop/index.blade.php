611efac4896947143fb9597a1fb3757751d30a3a
marcinosiak <om77@wp.pl>
2017-07-27 13:56:58 +0200

    Wyświetlenie produktów pobranych z bazy

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,93 +5,25 @@
 @endsection
 
 @section('content')
-    <div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
+
+    @foreach ($products->chunk(3) as $productChunk)
+      <div class="row">
+
+        @foreach ($productChunk as $product)
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <img class="img-responsive thumbnail" src="{{ $product->imagePath }}" alt="...">
+              <div class="caption">
+                <h3>{{ $product->title }}</h3>
+                <p class="description">{{ $product->description }}</p>
+                <div class="pull-left price">{{ $product->price }} zł</div>
+                <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
+              </div>
             </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
           </div>
-        </div>
+        @endforeach
+
       </div>
+    @endforeach
 
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
-            </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
-            </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
-            </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
-            </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img class="img-responsive thumbnail" src="https://images-na.ssl-images-amazon.com/images/I/51ggKJIFAjL._SX350_BO1,204,203,200_.jpg" alt="...">
-          <div class="caption">
-            <h3>Nazwa produktu</h3>
-            <p class="description">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
-            <div class="pull-left price">
-              20,00 zł
-            </div>
-            <div class="clearfix"><a href="#" class="btn btn-default pull-right btn-success" role="button">Dodaj do koszyka</a></div>
-          </div>
-        </div>
-      </div>
-
-    </div>    
 @endsection
