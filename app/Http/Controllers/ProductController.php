5af07fc221e146ee124262be43f12e703aaf05dc
marcinosiak <om77@wp.pl>
2017-08-09 15:27:54 +0200

    Zapis zamówienia do bazy po dokonaniu płatności

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Cart;
 use App\Product;
+use App\Order;
 use Session;
 use Stripe\Charge;
 use Stripe\Stripe;
+use Auth;
 
 class ProductController extends Controller
 {
@@ -88,12 +90,21 @@ class ProductController extends Controller
       Stripe::setApiKey('sk_test_CuHOLCWWQZtcW6A7uc16TkrS');
 
       try {
-        Charge::create(array(
+        $charge = Charge::create(array(
           "amount" => $cart->totalPrice * 100,
           "currency" => "usd",
           "source" => $request->input('stripeToken'), // obtained with Stripe.js
           "description" => "Test Charge"
         ));
+
+        $order = new Order();
+        $order->cart = serialize($cart);
+        $order->address = $request->input('address');
+        $order->name = $request->input('name');
+        $order->payment_id = $charge->id;
+
+        Auth::user()->orders()->save($order);
+
       } catch (\Exception $e) {
         return redirect()->route('checkout')->with('error', $e->getMessage());
       }
